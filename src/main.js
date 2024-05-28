ffd1727f0f8cd0a824026da482cb66c9133461a0
fort119 <i6316434@gmail.com>
2024-05-29 03:09:29 +0400

    ht12

--- src/main.js
+++ src/main.js
@@ -1,71 +1,90 @@
-///first programm
-function fn1(x) {
-
-  if (arguments.length == 0) {
-    x = 1;
-  }
-  return function (y) {
-    alert(x + y);
-  }
+// first programm
+function checkFn(a, b) {
+  alert(a * b);
 }
 
-let fn2 = fn1(6);
-fn2(5);
+function makeLogging(fn) {
+  let log = [];
+  function wrapper() {
+    log.push([].slice.call(arguments));
+    console.log(log);
+    return fn.apply(this, arguments);
+  }
+
+  return wrapper;
+}
+
+
+logDec = makeLogging(checkFn);
+
+logDec(6, 7);
+logDec(8, 9);
+logDec(10, 11);
+logDec(12, 13);
 
 // second programm
-function counter(startValue, step) {
-  let count = startValue;
 
-  function increment() {
-    count += step;
-    console.log(count);
+function checkNumber(value) {
+  return typeof value == 'number';
+}
+
+function typeCheck(fn, validator) {
+  return function () {
+    for (let i = 0; i < arguments.length; i++) {
+      if (!validator(arguments[i])) {
+        throw ("argument " + (i + 1) + " is incorrect");
+      }
+    }
+    return fn.apply(this, arguments);
   }
+}
 
-  function decrement() {
-    count -= step;
-    console.log(count);
-  }
+function mult(a, b) {
+  return a * b;
+}
 
-  function reset() {
-    count = 0;
-    console.log(count);
+mult = typeCheck(mult, checkNumber);
 
-  }
+console.log(mult(6, 5));
 
-  return {
-    increment: increment,
-    decrement: decrement,
-    reset: reset,
+mult(4, "ff");
+console.log(mult(4, 3));
+
+///third programm программа даёт ограничение на неправильный ввод
+function retry(fn, maxAttempts) {
+  return function (...args) {
+    let attempts = 0;
+    while (attempts < maxAttempts) {
+      try {
+        return fn(...args);
+      } catch (error) {
+        attempts++;
+        console.log(`Попытка ${attempts} из ${maxAttempts} провалилась: ${error.message}. Сейчас пройдёт следующая`);
+        if (attempts === maxAttempts) {
+          throw error;
+        }
+      }
+    }
   };
 }
 
-let myCounter = counter(5, 4);
+const test1 = retry(function (x) {
+  if (x == 0) {
+    throw new Error("результат вычисления некорректный");
+  }
+  return x;
+}, 6);
 
-myCounter.increment();
-myCounter.increment();
-myCounter.decrement();
-myCounter.increment()
-myCounter.increment()
-myCounter.reset()
-
-///third programm
-function sequence(...funcs) {
-  return function (startValue) {
-    return funcs.reduce((result, func) => func(result), startValue);
-  };
+try {
+  const result = test1(mult(4, 0));
+  console.log(result);
+} catch (error) {
+  console.log("достигнуто максимальное количество попыток");
 }
 
-function f1(x) {
-  return x + 1;
+try {
+  const result = test1(mult(4, 10));
+  console.log(result);
+} catch (error) {
+  console.log("достигнуто максимальное количество попыток");
 }
-
-function f2(x) {
-  return x * 2;
-}
-
-function f3(x) {
-  return x - 3;
-}
-
-const seqFunc = sequence(f1, f2, f3);
-console.log(seqFunc(13)); 
